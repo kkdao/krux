5955b460992047180d075fc1a9840604888ad3b4
kkdao <kkdao@protonmail.com>
2024-12-09 22:12:18 -0500

    BW fix

--- src/krux/wallet.py
+++ src/krux/wallet.py
@@ -260,7 +260,8 @@ def parse_wallet(wallet_data, allow_assumption=None):
             keys = []
             for i in range(len(key_vals)):
                 xpub = key_vals[i]
-                if xpub.lower().startswith("xpub") or xpub.lower().startswith("tpub"):
+                xpub_prefix = xpub[:4].lower()
+if xpub_prefix[1:] == "pub" and xpub_prefix[0] in ["x", "z", "t", "v"]:
                     fingerprint = key_vals[i - 1]
                     keys.append((xpub, fingerprint))
 
